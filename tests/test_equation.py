9a80a5c4c4b34012512b9b84be2cc81a1df7b734
Fanda Blahoudek <fandikb@gmail.com>
2021-03-24 13:07:45 +0100

    Use pytest

--- tests/test_equation.py
+++ tests/test_equation.py
@@ -2,11 +2,15 @@ from noodler import StringEquation
 
 e = StringEquation("xyz", "xyxy")
 
-assert e.vars == {"x", "y", "z"}
-assert e.get_side("left") == e.left
-assert e.get_side("right") == e.right
 
-assert e.switched.vars == e.vars
-assert e.switched.switched == e
-assert e.left == e.switched.right
-assert e.right == e.switched.left
+def test_sides():
+    assert e.vars == {"x", "y", "z"}
+    assert e.get_side("left") == e.left
+    assert e.get_side("right") == e.right
+
+
+def test_switched():
+    assert e.switched.vars == e.vars
+    assert e.switched.switched == e
+    assert e.left == e.switched.right
+    assert e.right == e.switched.left
