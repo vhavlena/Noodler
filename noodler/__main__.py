069ce5ff4940fd2b48d224ddb836c411870f2101
Fanda Blahoudek <fandikb@gmail.com>
2021-06-23 10:57:13 +0200

    Add __main__ and setup.py for external execution.

---
+++ noodler/__main__.py
@@ -0,0 +1,37 @@
+import argparse
+import sys
+
+from .core import is_straightline
+from .parser import SmtlibParserHackAbc
+from .noodler import StraightlineNoodleMachine
+
+
+def main(args: argparse.Namespace):
+    filename = args.filename
+    bidi = args.bidi
+
+    smt_parser = SmtlibParserHackAbc(filename)
+    q = smt_parser.parse_query()
+    assert is_straightline(q.equations)
+    noodler_machine = StraightlineNoodleMachine(q)
+
+    if args.propagate_vars and not bidi:
+        noodler_machine.propagate_contraints()
+
+    if not args.parse_only:
+        res = "sat" if noodler_machine.is_sat(bidi) else "unsat"
+        print(res)
+
+
+description = """Solves SAT problem for string constraints."""
+
+# TODO add helper texts.
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("--parse_only", action="store_true")
+parser.add_argument("--propagate-vars", action="store_true")
+parser.add_argument("--bidi", action="store_true")
+parser.add_argument("filename", type=str)
+
+args = parser.parse_args()
+main(args)
+exit(0)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='noodler',
+    version='0.0.1',
+    packages=['noodler'],
+    url='',
+    license='',
+    author='Fanda Blahoudek',
+    author_email='fandikb+dev@gmail.com',
+    description='Use automata noodles to solve string constraints'
+)
