3e04435e75fdd64b55eb1cab49c740f387a7b15b
David Chocholatý <chocholaty.david@protonmail.com>
2022-08-31 11:43:34 +0200

    Reflect changes in intersection function signature

--- noodler/noodler.py
+++ noodler/noodler.py
@@ -103,7 +103,7 @@ def create_unified_query(equation: StringEquation,
         aut_l = {left_auts[i] for i in equation.indices_l[var]}
         aut_r = {right_auts[i] for i in equation.indices_r[var]}
         var_auts = aut_l.union(aut_r)
-        product: Aut = multiop(list(var_auts), lambda x,y: mata.Nfa.intersection(x,y)[0])
+        product: Aut = multiop(list(var_auts), lambda x,y: mata.Nfa.intersection(x,y))
         product.trim()
         const[var] = mata.Nfa.reduce(product)[0]
         if const[var].get_num_of_states() == 0:
